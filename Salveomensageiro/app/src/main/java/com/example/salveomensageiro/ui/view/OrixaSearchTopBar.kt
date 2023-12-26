4ffb97eef6ec0fe3851b1eca54cc4fd24b7aa9b1
Crizza <ivan.alineri@outlook.com>
2023-12-26 20:28:02 -0300

    melhorias de import e preview da home

--- Salveomensageiro/app/src/main/java/com/example/salveomensageiro/ui/view/OrixaSearchTopBar.kt
+++ Salveomensageiro/app/src/main/java/com/example/salveomensageiro/ui/view/OrixaSearchTopBar.kt
@@ -1,6 +1,5 @@
 package com.example.salveomensageiro.ui.view
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -20,7 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OrixaSearchTopBar(
     modifier: Modifier = Modifier,
--- Salveomensageiro/app/src/main/java/com/example/salveomensageiro/ui/view/OrixasHome.kt
+++ Salveomensageiro/app/src/main/java/com/example/salveomensageiro/ui/view/OrixasHome.kt
@@ -16,8 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.salveomensageiro.data.Orixa
 import com.example.salveomensageiro.ui.viewmodel.OrixaViewmodelState
 import com.example.salveomensageiro.ui.viewmodel.OrixasViewmodel
@@ -33,9 +35,9 @@ fun OrixasHome(
     val orixas = orixasViewmodel.state.collectAsState().value
     when (val state = orixas) {
         is OrixaViewmodelState.GetOrixas -> SetScafold(
-            modifier,
-            state.orixa,
-            navController,
+            modifier = modifier,
+            orixasList = state.orixa,
+            navController = navController,
             orixasViewmodel = orixasViewmodel
         )
 
@@ -71,6 +73,55 @@ fun SetScafold(
             orixasList = orixasList,
             navController = navController
         )
+    }
+}
+
+@Preview
+@Composable
+fun HomePreviewWithScafold() {
+
+}
+
+@Preview
+@Composable
+fun HomeCardPreview() {
+    val nav = rememberNavController()
+    val ls = listOf(
+        Orixa(
+            name = "Nanã",
+            imageUrl = "https://ocandomble.files.wordpress.com/2008/04/nana.jpg?w=216&h=300",
+            day = "",
+            color = "",
+            symbols = "",
+            elements = "",
+            know = "",
+            greetings = ""
+        )
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            itemsIndexed(ls, key = { _, item ->
+                item.name
+            }
+            ) { index, item ->
+                val orixaInfo = Orixa(item.name, item.imageUrl)
+                ItemOrixa(
+                    orixaInfo = orixaInfo,
+                    onItemClick = {
+                        nav.navigate("detailCard/${index}")
+                    }
+                )
+            }
+        }
     }
 }
 
